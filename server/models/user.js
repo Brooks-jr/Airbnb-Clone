5bf680904dda4591a157dae7538371d2657fae00
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-10-27 11:11:45 -0500

    defined twice, one removed

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-var Schema = mongoose.Schema;
+
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
